51f5e9eddc88160258ffa77a4ac847b00180b151
alexvia <alexvia.software@gmail.com>
2017-02-05 10:25:21 +0100

    progress

--- src/odin.cpp
+++ src/odin.cpp
@@ -44,21 +44,51 @@ void LoadShaders(const char *Path)
 extern "C" __declspec(dllexport) void __stdcall Init(Game_State *State)
 {
 	u64 Size;
-	State->services.GetFileSize("Shaders/simple.vs", &Size);
-	State->services.ReadEntireFile("Shaders/simple.vs", State->Memory, Size);
-	GLuint Shader = glCreateShader(GL_VERTEX_SHADER);
-	const GLchar *const source = (const GLchar *const)State->Memory;
-	glShaderSource(Shader, 1, &source, 0);
-	glCompileShader(Shader);
+	State->Services.GetFileSize("Shaders/simple.vs", &Size);
+	State->Services.ReadEntireFile("Shaders/simple.vs", State->Memory, Size);
+	GLuint VShader = glCreateShader(GL_VERTEX_SHADER);
+	const GLchar *source = (const GLchar *)State->Memory;
+	glShaderSource(VShader, 1, &source, 0);
+	glCompileShader(VShader);
 
 	GLint Success;
 	GLchar InfoLog[512];
-	glGetShaderiv(Shader, GL_COMPILE_STATUS, &Success);
+	glGetShaderiv(VShader, GL_COMPILE_STATUS, &Success);
 	if (!Success)
 	{
-		glGetShaderInfoLog(Shader, 512, 0, InfoLog);
+		glGetShaderInfoLog(VShader, 512, 0, InfoLog);
 		OutputDebugString(InfoLog);
 	}
+
+	State->Services.GetFileSize("Shaders/simple.fs", &Size);
+	State->Services.ReadEntireFile("Shaders/simple.fs", State->Memory, Size);
+	State->Memory[Size] = 0;
+	GLuint FShader = glCreateShader(GL_FRAGMENT_SHADER);
+	source = (const GLchar *const)State->Memory;
+	glShaderSource(FShader, 1, &source, 0);
+	glCompileShader(FShader);
+
+	glGetShaderiv(FShader, GL_COMPILE_STATUS, &Success);
+	if (!Success)
+	{
+		glGetShaderInfoLog(FShader, 512, 0, InfoLog);
+		OutputDebugString(InfoLog);
+	}
+
+	GLuint Program = glCreateProgram();
+	glAttachShader(Program, VShader);
+	glAttachShader(Program, FShader);
+	glLinkProgram(Program);
+
+	glGetProgramiv(Program, GL_LINK_STATUS, &Success);
+	if (!Success)
+	{
+		glGetProgramInfoLog(Program, 512, 0, InfoLog);
+		OutputDebugString(InfoLog);
+	}
+
+	glDeleteShader(VShader);
+	glDeleteShader(FShader);
 
 	// ----- Resize(SCRWIDTH, SCRHEIGHT); ------
 	glViewport(0, 0, State->Width, State->Height);
